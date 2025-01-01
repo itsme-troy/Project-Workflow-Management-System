d598d8746a471ea7d5eeff7f60b41beb0cdc3804
itsme-troy <teodoroleoberttroy19@gmail.com>
2025-01-01 23:49:16 +0800

    default pic fix

--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -123,15 +123,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 # STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static') ]
 STATICFILES_DIRS = [
     BASE_DIR / "project/static",  # Adjust this line if necessary
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
-MEDIA_URL = 'submissions/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'submissions')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
