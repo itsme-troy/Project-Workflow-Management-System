09fb6f0c3f03ebb322be00248fe9bd9fdc5bb803
itsme-troy <teodoroleoberttroy19@gmail.com>
2025-01-14 00:07:55 +0800

    revised dashboard css

--- cms/cms/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import consumers
-
-websocket_urlpatterns = [
-    path('ws/project_ideas/', consumers.ProjectIdeasConsumer.as_asgi()),
-]
--- cms/mutual_availability/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MutualAvailabilityConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "mutual_availability"
